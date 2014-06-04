cfd661f2d735c81c9acce9645aeb8178fa56bfe8
mfcndw <mfcndw@163.com>
2014-06-03 21:13:42 -0400

    xml

--- Task 15/src/bean/TestBean.java
+++ Task 15/src/bean/TestBean.java
@@ -3,5 +3,14 @@ package bean;
 import org.mybeans.form.FormBean;
 
 public class TestBean extends FormBean{
+	private String test;
 
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
 }
