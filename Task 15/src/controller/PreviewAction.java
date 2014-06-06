12b03c8d563942e67dff68b269fabc528f865755
Hao Ge <gehao868@gmail.com>
2014-06-05 23:32:11 -0400

    hge

--- Task 15/src/controller/PreviewAction.java
+++ Task 15/src/controller/PreviewAction.java
@@ -1,6 +1,12 @@
 package controller;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
+
+import parser.XmlParser;
 
 public class PreviewAction extends Action {
 
@@ -11,6 +17,18 @@ public class PreviewAction extends Action {
 
 	@Override
 	public String perform(HttpServletRequest request) {
+		
+		XmlParser xml = new XmlParser();
+		File newXmlFile;
+		HashMap<String, String> hp = new HashMap<String, String>();
+		try {
+			newXmlFile = xml.saveXml(request, "test.xml");
+			hp = xml.importXml(newXmlFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(hp.size());
 		
 		StringBuffer filePath = request.getRequestURL();
 
